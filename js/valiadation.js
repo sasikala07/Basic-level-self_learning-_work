87cc9d3fae7d977fec96ed6af16d53648dc52373
sasikala sivaraman <72785420+sasikala07@users.noreply.github.com>
2020-10-26 13:50:34 +0530

    Update valiadation.js

--- js/valiadation.js
+++ js/valiadation.js
@@ -1,41 +1,43 @@
 function validate(){
    
     var name=document.getElementById("name")
-    if(name.value==""){
+//     if(name.value==""){
      
   
-        alert("name field cannot be empty")
-        //document.write("The page cannot be empty")
+//         alert("name field cannot be empty")
+//         //document.write("The page cannot be empty")
+//         name.focus
+//         return false //dont execute submit button
+  //  }
+    if (name.value==null || name.value=="") { 
+        document.getElementById('new').innerHTML = " name field cannot be empty" 
         name.focus
-        return false //dont execute submit button
-    }
-    if (name==null || name=="") { 
-        document.getElementById('new').innerHTML = " name must be filled out" 
         return false
   } 
   
   
     var email=document.getElementById("email")
-    if(email.value==""){
-        alert("email field cannot be empty")
-        email.focus
-        return false //dont execute submit button
-    }
-    if(email.value==""){
-      alert("email field cannot be empty")
-      email.focus
-      return false //dont execute submit button
-  
-    }
+//     if(email.value==""){
+//         alert("email field cannot be empty")
+//         email.focus
+//         return false //dont execute submit button
+//     }
+   if (name.value==null || name.value=="") { 
+        document.getElementById('new1').innerHTML = " email field cannot be empty" 
+        name.focus
+        return false
+  } 
   
     var number=document.getElementById("mobile")
     if(number.value==""){
-      alert("mobile number cannot be empty")
+        document.getElementById('new2').innerHTML = " mobile field cannot be empty"
+      //alert("mobile number cannot be empty")
       number.focus
       return false //dont execute submit button
     }
     if(number.value.length!=10 || isNaN (number.value)){
-      alert("inavalid number")
+       document.getElementById('new2').innerHTML = " Invalid"
+       //alert("inavalid number")
       number.focus
       return false //dont execute submit button to not save 
   }
